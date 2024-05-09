3203c4494cee1ac4bc75640a60114096db44ee54
Yunus Emre Deniz <yunusemredenz@hotmail.com>
2024-05-09 21:23:40 +0300

    sign-up bitti auth dosyası kaldırıldı

--- app/patient-record/page.tsx
+++ app/patient-record/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import MantineDrawer from "../../../components/MantineDrawer"
-import MantineSearch from "../../../components/MantineSearch"
+import MantineDrawer from "../../components/MantineDrawer"
+import MantineSearch from "../../components/MantineSearch"
 export default function HomePage() {
   return <div style={{ display: "flex" }}>
     <MantineDrawer></MantineDrawer>
